bc308d807685368f297fdc537fa111086e196e42
Carol Lee <CLee@thrillworks.com>
2021-01-31 22:58:43 -0500

    Removed cookie and chaining auth and search call on client

--- src/hooks/useFatsecret.js
+++ src/hooks/useFatsecret.js
@@ -7,18 +7,31 @@ const useFatsecret = () => {
   const [foods, setFoods] = useState([]);
 
   const search = async (term) => {
-    const response = await axios.get("/api", {
-      // headers: {
-      //   "Content-Type": "application/json",
-      //   Authorization: `Bearer ${token}`,
-      // },
-      params: {
-        search_expression: term,
-        method: "foods.search",
-        format: "json",
-      },
-    });
-    console.log(response.data);
+    const response = await axios
+      .get("/auth")
+      .then((res) => res.data)
+      .then((data) => {
+        return axios.get("/api", {
+          params: {
+            search_expression: term,
+            method: "foods.search",
+            format: "json",
+            token: data,
+          },
+        });
+      });
+    // const response = await axios.get("/api", {
+    //   // headers: {
+    //   //   "Content-Type": "application/json",
+    //   //   Authorization: `Bearer ${token}`,
+    //   // },
+    //   params: {
+    //     search_expression: term,
+    //     method: "foods.search",
+    //     format: "json",
+    //   },
+    // });
+    console.log(response);
     setFoods(response.data.foods.food);
   };
 
